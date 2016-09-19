bdb8cca193536888c9154e4ff5dd74b6faa71422
melodie-poncet <melodie.poncet@gmail.com>
2016-09-19 13:43:09 +0200

    commit temp pour changement de branche

---
+++ src/main/java/com/api/tuto/domain/Dresseur.java
@@ -0,0 +1,39 @@
+package com.api.tuto.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "dresseur")
+public class Dresseur {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nom")
+	private String nom;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/api/tuto/repository/DresseurRepository.java
@@ -0,0 +1,9 @@
+package com.api.tuto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.tuto.domain.Dresseur;
+
+public interface DresseurRepository extends JpaRepository<Dresseur, Long>{
+
+}
