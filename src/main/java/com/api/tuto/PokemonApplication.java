77ab49137282ac896a4b254f876061ec6078b2fe
melodie-poncet <melodie.poncet@gmail.com>
2016-09-19 10:11:57 +0200

    swagger doc done

--- src/main/java/com/api/tuto/PokemonApplication.java
+++ src/main/java/com/api/tuto/PokemonApplication.java
@@ -3,7 +3,10 @@ package com.api.tuto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PokemonApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/api/tuto/PokemonResource.java
+++ src/main/java/com/api/tuto/PokemonResource.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import com.api.tuto.domain.Pokemon;
 import com.api.tuto.repository.PokemonRepository;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/pokedex")
 public class PokemonResource {
@@ -22,6 +25,8 @@ public class PokemonResource {
     @Autowired
     private PokemonRepository pokemonRepository;
 
+    @ApiOperation(value = "Gets all pokemons in pokedex.",  response = Pokemon.class, responseContainer = "List")
+    @ApiResponses(value = {@io.swagger.annotations.ApiResponse(code = 200, message = "Successful response") })
     @Transactional(readOnly = true)
     @RequestMapping(path="/pokemons", method = RequestMethod.GET)
     @CrossOrigin(origins ="http://editor.swagger.io")
@@ -30,6 +35,8 @@ public class PokemonResource {
         return pokemons;
     }
 
+    @ApiOperation(value = "Get pokemon with ID.",  response = Pokemon.class)
+    @ApiResponses(value = {@io.swagger.annotations.ApiResponse(code = 200, message = "Successful response") })
     @RequestMapping(path ="/pokemons/{id}", method = RequestMethod.GET)
     @Transactional(readOnly = true)
     ResponseEntity<Pokemon> getPokemon(@PathVariable Long id) {
@@ -41,6 +48,8 @@ public class PokemonResource {
         }
     }
 
+    @ApiOperation(value = "Create a new pokemon",  response = Pokemon.class, notes = "the ID must match the official number of pokemon ")
+    @ApiResponses(value = {@io.swagger.annotations.ApiResponse(code = 200, message = "Successful response") })
     @RequestMapping(value = "/pokemons", method = RequestMethod.POST)
     public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon) throws URISyntaxException {
     	Pokemon result = pokemonRepository.save(pokemon);
@@ -48,7 +57,8 @@ public class PokemonResource {
         return ResponseEntity.created(newRessourceURI).body(pokemon);
     }
 
-
+    @ApiOperation(value = "Update a pokemon with ID",  response = Pokemon.class, notes = "ID does must not change, it could be a problem")
+    @ApiResponses(value = {@io.swagger.annotations.ApiResponse(code = 200, message = "Successful response") })
     @RequestMapping(value = "/pokemons/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Pokemon> updateTask(@PathVariable Long id, @RequestBody Pokemon pokemon) throws URISyntaxException {
     	pokemon.setId(id);
@@ -56,6 +66,8 @@ public class PokemonResource {
         return ResponseEntity.ok().body(result);
     }
 
+    @ApiOperation(value = "Delete a pokemon with ID",  response = Pokemon.class)
+    @ApiResponses(value = {@io.swagger.annotations.ApiResponse(code = 200, message = "Successful response") })
     @RequestMapping(value = "/pokemons/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) throws URISyntaxException {
         pokemonRepository.delete(id);
